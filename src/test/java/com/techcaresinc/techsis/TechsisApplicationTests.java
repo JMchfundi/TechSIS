4d79096ce989cb5815bc6d1b68db09b36928c0db
JMchfundi <akidamjaffar@gmail.com>
2019-12-23 15:18:56 +0300

    TechSIS - Techcares Inc School Information System (Initial commit)

---
+++ src/test/java/com/techcaresinc/techsis/TechsisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techcaresinc.techsis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechsisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
